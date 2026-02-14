e2b21691a42f0d36705626e2e944d3effd29a245
Insality <insality@gmail.com>
2026-02-14 15:31:49 +0200

    Update

---
+++ example/assets/hexgrid/entities.go
@@ -0,0 +1,84 @@
+embedded_components {
+  id: "tile_dirt"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_dirt.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_dirt_2"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_dirt_2.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_dirt_3"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_dirt_3.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_grass"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_grass.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_grass_2"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_grass_2.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_grass_3"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_grass_3.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_sand"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_sand.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_sand_2"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_sand_2.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_sand_3"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_sand_3.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_stone"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_stone.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_stone_2"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_stone_2.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile_stone_3"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/tile_stone_3.go\"\n"
+  ""
+}
+embedded_components {
+  id: "r_tree_round"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/r_tree_round.go\"\n"
+  ""
+}
+embedded_components {
+  id: "r_tree_round_2"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/r_tree_round_2.go\"\n"
+  ""
+}
---
+++ example/assets/hexgrid/entities/tile_sand_2.go
@@ -0,0 +1,14 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"tile_sand_2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/example/assets/hexgrid/hexgrid_tiles.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: -20.0
+  }
+}
