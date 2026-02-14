b614920dd942eeb2f979e2b3a1eb5a56c1830ca0
Insality <insality@gmail.com>
2026-02-14 22:06:28 +0200

    Update

--- example/assets/isogrid/entities.go
+++ example/assets/isogrid/entities.go
@@ -526,3 +526,15 @@ embedded_components {
   data: "prototype: \"/example/assets/isogrid/entities/waterW.go\"\n"
   ""
 }
+embedded_components {
+  id: "r_tree_round"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/r_tree_round.go\"\n"
+  ""
+}
+embedded_components {
+  id: "r_tree_round_2"
+  type: "factory"
+  data: "prototype: \"/example/assets/hexgrid/entities/r_tree_round_2.go\"\n"
+  ""
+}
