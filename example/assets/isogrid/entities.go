c301db73c6d02750dc8fa612a9d54c4e080f08ff
Insality <insality@gmail.com>
2026-02-14 18:16:10 +0200

    Isogrid example

---
+++ example/assets/isogrid/entities.go
@@ -0,0 +1,528 @@
+embedded_components {
+  id: "beach"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beach.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachCornerES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachCornerES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachCornerNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachCornerNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachCornerNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachCornerNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachCornerSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachCornerSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "beachW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/beachW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "bridgeEW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/bridgeEW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "bridgeNS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/bridgeNS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "crossroad"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/crossroad.go\"\n"
+  ""
+}
+embedded_components {
+  id: "crossroadESW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/crossroadESW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "crossroadNES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/crossroadNES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "crossroadNEW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/crossroadNEW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "crossroadNSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/crossroadNSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "dirt"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/dirt.go\"\n"
+  ""
+}
+embedded_components {
+  id: "dirtDouble"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/dirtDouble.go\"\n"
+  ""
+}
+embedded_components {
+  id: "endE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/endE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "endN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/endN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "endS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/endS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "endW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/endW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "exitE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/exitE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "exitN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/exitN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "exitS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/exitS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "exitW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/exitW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "grass"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/grass.go\"\n"
+  ""
+}
+embedded_components {
+  id: "grassWhole"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/grassWhole.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "hillW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/hillW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "lotW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/lotW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverBankedES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverBankedES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverBankedEW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverBankedEW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverBankedNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverBankedNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverBankedNS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverBankedNS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverBankedNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverBankedNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverBankedSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverBankedSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverEW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverEW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverNS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverNS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "riverSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/riverSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "road"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/road.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadEW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadEW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHill2E"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHill2E.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHill2N"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHill2N.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHill2S"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHill2S.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHill2W"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHill2W.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHillE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHillE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHillN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHillN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHillS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHillS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadHillW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadHillW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadNS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadNS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "roadSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/roadSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "water"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/water.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterCornerES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterCornerES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterCornerNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterCornerNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterCornerNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterCornerNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterCornerSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterCornerSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterES"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterES.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterN"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterN.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterNE"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterNE.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterNW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterNW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterS"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterS.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterSW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterSW.go\"\n"
+  ""
+}
+embedded_components {
+  id: "waterW"
+  type: "factory"
+  data: "prototype: \"/example/assets/isogrid/entities/waterW.go\"\n"
+  ""
+}
