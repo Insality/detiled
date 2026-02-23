f1b50db6ab1eb7f6f21ca8b306cefa11081ae972
Insality <insality@gmail.com>
2026-02-23 20:09:42 +0200

    Add grid game objects example

---
+++ example/example_grid_game_objects/assets/entities.go
@@ -0,0 +1,48 @@
+embedded_components {
+  id: "cactus"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/cactus.go\"\n"
+  ""
+}
+embedded_components {
+  id: "icon_barrel"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/icon_barrel.go\"\n"
+  ""
+}
+embedded_components {
+  id: "icon_chest"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/icon_chest.go\"\n"
+  ""
+}
+embedded_components {
+  id: "icon_key"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/icon_key.go\"\n"
+  ""
+}
+embedded_components {
+  id: "icon_stop"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/icon_stop.go\"\n"
+  ""
+}
+embedded_components {
+  id: "icon_table"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/icon_table.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tree"
+  type: "factory"
+  data: "prototype: \"/example/assets/grid/entities/tree.go\"\n"
+  ""
+}
+embedded_components {
+  id: "tile"
+  type: "factory"
+  data: "prototype: \"/example/example_grid_game_objects/assets/objects/tile.go\"\n"
+  ""
+}
